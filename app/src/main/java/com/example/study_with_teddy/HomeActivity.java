933b940c21afe9a423feb48794f998d71546781d
Chathum10 <chathumsanga65@gmail.com>
2021-09-26 01:56:52 +0530

    Update HomeActivity.java

--- app/src/main/java/com/example/study_with_teddy/HomeActivity.java
+++ app/src/main/java/com/example/study_with_teddy/HomeActivity.java
@@ -21,6 +21,7 @@ public class HomeActivity extends AppCompatActivity {
         getStarted.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //cha
                 startActivity(new Intent(HomeActivity.this,Menu.class));
 
             }
