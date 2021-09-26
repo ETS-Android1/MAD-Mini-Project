0d4ea477ba3783acd4c30b03b27d96489d82f424
Pawani Muthusala Perera <pawaniperera20@gmail.com>
2021-09-26 15:53:50 +0530

    update

--- app/src/main/java/com/example/study_with_teddy/ALLsettingsMain.java
+++ app/src/main/java/com/example/study_with_teddy/ALLsettingsMain.java
@@ -1,14 +1,27 @@
 package com.example.study_with_teddy;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class ALLsettingsMain extends AppCompatActivity {
+    private Button guide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cgmpsettings_main);
+        setContentView(R.layout.activity_allsettingsmain);
+        guide = findViewById(R.id.button);
+
+        guide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ALLsettingsMain.this, listSettings.class));
+
+            }
+        });
     }
 }
