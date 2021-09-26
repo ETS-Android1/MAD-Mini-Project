77ebb0212225f219e6dd6f4bdfbc69067f71fec2
Gangamini Athmaja <89349666+GangaminiAthmaja@users.noreply.github.com>
2021-09-26 16:07:19 -0700

    update

--- app/src/main/java/com/example/study_with_teddy/ALLsettingsMain.java
+++ app/src/main/java/com/example/study_with_teddy/ALLsettingsMain.java
@@ -1,6 +1,7 @@
 package com.example.study_with_teddy;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class ALLsettingsMain extends AppCompatActivity {
     private Button guide;
+    private Button youtube;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +25,19 @@ public class ALLsettingsMain extends AppCompatActivity {
 
             }
         });
+
+        youtube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl("https://www.youtube.com/");
+            }
+
+        });
+    }
+
+    private void gotoUrl(String s){
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));
+
     }
 }
